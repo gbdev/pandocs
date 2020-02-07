01d4203b0f83d446b3287ddfb643392001e51f34
Antonio Vivace <avivace4@gmail.com>
2020-02-07 03:05:43 +0100

    Copy single articles and assets

--- render/merge.sh
+++ render/merge.sh
@@ -3,3 +3,8 @@ cd ../content
 markdown-pp index.mdpp -o ../render/index.md -e latexrender
 cd -
 
+echo "Copying single non-templated articles.."
+cp ../content/Timer_Obscure_Behaviour.md .
+
+echo "Copying image assets.."
+cp ../content/imgs .vuepress/public/ -r
