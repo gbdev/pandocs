c0b16d5533f32409cd3ecfb1e7a3bac24e40c965
Antonio Vivace <avivace4@gmail.com>
2020-02-07 18:00:27 +0100

    Activate and deactivate the venv inside the script. Verbose copy

--- render/merge.sh
+++ render/merge.sh
@@ -1,10 +1,14 @@
+source bin/activate.fish
+
 echo "Producing the merged markdown file.."
 cd ../content
 markdown-pp index.mdpp -o ../render/index.md -e latexrender
 cd -
 
 echo "Copying single non-templated articles.."
-cp ../content/Timer_Obscure_Behaviour.md .
+cp ../content/Timer_Obscure_Behaviour.md . --verbose 
 
 echo "Copying image assets.."
-cp ../content/imgs .vuepress/public/ -r
+cp ../content/imgs .vuepress/public/ -r --verbose
+
+deactivate
