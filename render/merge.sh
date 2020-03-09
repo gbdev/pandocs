2afb2add742d0ca38d7cabb34d3ebde9cb0dd0ff
Antonio Vivace <avivace4@gmail.com>
2020-03-09 01:52:50 +0100

    Do not touch the virtualenv

--- render/merge.sh
+++ render/merge.sh
@@ -1,5 +1,3 @@
-source bin/activate.fish
-
 echo "Producing the merged markdown file.."
 cd ../content
 markdown-pp index.mdpp -o ../render/index.md -e latexrender
@@ -10,5 +8,3 @@ cp ../content/Timer_Obscure_Behaviour.md . --verbose
 
 echo "Copying image assets.."
 cp ../content/imgs .vuepress/public/ -r --verbose
-
-deactivate
