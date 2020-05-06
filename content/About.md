18fc351fa11fe2b253f74ad91679771e8ed49feb
Antonio Vivace <avivace4@gmail.com>
2020-05-06 21:19:34 +0200

    Include build information in the final document (#28)

--- content/About.md
+++ content/About.md
@@ -1,3 +1,4 @@
 This is a new, experimental version of Pan Docs, mantained in the Markdown format.
 
 To learn more about the history and the mission of the project, check the [README](https://github.com/gbdev/pandocs#history).
+
--- render/merge.sh
+++ render/merge.sh
@@ -1,6 +1,19 @@
-echo "Producing the merged markdown file.."
+commithash=$(git rev-parse HEAD)
+shortcommithash=$(git rev-parse --short HEAD)
+timestamp=$(git show -s --format=%ci HEAD)
+
 cd ../content
+cp About.md About2.md
+
+echo "This document version was produced from git commit ["$shortcommithash"](https://github.com/gbdev/pandocs/tree/"$commithash") ("$timestamp")." >> About.md
+
+echo "Producing the merged markdown file.."
+
 markdown-pp index.mdpp -o ../render/index.md -e latexrender
+
+echo "Restoring untemplated version of About.md"
+mv About2.md About.md
+
 cd -
 
 echo "Copying single non-templated articles.."
