16f80039c1c07a0b128cf88cd3cfa6f8198c4749
Antonio Vivace <avivace4@gmail.com>
2020-06-28 03:17:09 +0200

    Fix imgs location

--- render/merge.sh
+++ render/merge.sh
@@ -20,4 +20,4 @@ echo "Copying single non-templated articles.."
 cp ../content/Timer_Obscure_Behaviour.md . --verbose 
 
 echo "Copying image assets.."
-cp ../content/imgs .vuepress/public/ -r --verbose
+cp ../content/imgs .vuepress/public/imgs -r --verbose
