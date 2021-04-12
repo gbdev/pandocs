35e7c9ed8b1ec1bcec4c63d38fd36dbaa5660ea8
Antonio Vivace <antonio.vivace@cern.ch>
2021-04-12 19:05:24 +0200

    Formatting, print current job

--- content/imgs/src/graph_render.py
+++ content/imgs/src/graph_render.py
@@ -19,27 +19,31 @@ x_label_count = 10
 #  and in the same order as the Axis labels.
 # ------------------------------------------------------------------------------------------------
 graph_files = [
-    { "filename": "MBC5_Rumble_Mild.csv", "title": "'Mild Rumble'" },
-    { "filename": "MBC5_Rumble_Strong.csv", "title": "'Strong Rumble'" },
+    {"filename": "MBC5_Rumble_Mild.csv", "title": "Mild Rumble"},
+    {"filename": "MBC5_Rumble_Strong.csv", "title": "Strong Rumble"},
 ]
 # ================================================================================================
 
 
-
 def gen_graph(g_file):
     # Create Line Chart Object and Open File
-    chart = pygal.Line(show_dots=False, show_legend=False, show_minor_x_labels=False, x_label_rotation=x_label_rotation)
-    csv = open(g_file["filename"], 'r').readlines()
+    chart = pygal.Line(
+        show_dots=False,
+        show_legend=False,
+        show_minor_x_labels=False,
+        x_label_rotation=x_label_rotation,
+    )
+    csv = open(g_file["filename"], "r").readlines()
 
     # Set Chart and Axis Titles
     chart.title = g_file["title"]
-    headers = csv.pop(0).split(',')
+    headers = csv.pop(0).split(",")
     chart.x_title = headers[0]
     chart.y_title = headers[1]
 
     # Generate label spacing variables
-    min_x_val = float(csv[0].split(',')[0])
-    max_x_val = float(csv[len(csv)-1].split(',')[0])
+    min_x_val = float(csv[0].split(",")[0])
+    max_x_val = float(csv[len(csv) - 1].split(",")[0])
     x_mod_val = (max_x_val - min_x_val) / x_label_count
 
     # Generate graph data arrays
@@ -49,10 +53,10 @@ def gen_graph(g_file):
     last_x = None
     for line in csv:
         # Add data to label arrays
-        data = line.split(',')
+        data = line.split(",")
         x_labels.append(data[0])
         y_data.append(float(data[1]))
-        
+
         # Check if current X-Label should be Major Label
         xval_float = float(data[0])
         if last_x is not None and ((last_x % x_mod_val) > (xval_float % x_mod_val)):
@@ -63,10 +67,10 @@ def gen_graph(g_file):
     chart.x_labels = x_labels
     chart.x_labels_major = x_labels_major
     chart.add("", y_data)
-    chart.render_to_file('../' + g_file["filename"].replace('.csv', '.svg'))
-
+    chart.render_to_file("../" + g_file["filename"].replace(".csv", ".svg"))
 
 
 # Call gen_graph() for each file in the graph_files array
 for g_file in graph_files:
+    print(f"Generating {g_file['filename'].replace('.csv', '.svg')} ..")
     gen_graph(g_file)
