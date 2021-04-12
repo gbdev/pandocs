60eea123307e2069861e6e017ab29e75f932b09d
Hacktix <zecmad16@htlkaindorf.at>
2021-04-12 16:44:16 +0200

    Move graph title config to generator script + Rumble Filename Consistency

--- content/imgs/src/graph_render.py
+++ content/imgs/src/graph_render.py
@@ -14,26 +14,25 @@ x_label_count = 10
 # The script will automatically generate SVG line-graphs with the same file name
 # (replacing the .csv extension with .svg) within the /content/imgs directory
 #
-# The first line of the file must contain a title for the chart.
-# The second line must contain the X- and Y-Axis labels seperated by commas.
+# The first line of the file must contain the X- and Y-Axis labels seperated by commas.
 # The following lines are expected to contain the graph data in a comma-separated format
 #  and in the same order as the Axis labels.
 # ------------------------------------------------------------------------------------------------
 graph_files = [
-    "Rumble_Intense.csv",
-    "Rumble_Medium.csv"
+    { "filename": "MBC5_Rumble_Mild.csv", "title": "'Mild Rumble'" },
+    { "filename": "MBC5_Rumble_Strong.csv", "title": "'Strong Rumble'" },
 ]
 # ================================================================================================
 
 
 
-def gen_graph(filename):
+def gen_graph(g_file):
     # Create Line Chart Object and Open File
     chart = pygal.Line(show_dots=False, show_legend=False, show_minor_x_labels=False, x_label_rotation=x_label_rotation)
-    csv = open(filename, 'r').readlines()
+    csv = open(g_file["filename"], 'r').readlines()
 
     # Set Chart and Axis Titles
-    chart.title = csv.pop(0)
+    chart.title = g_file["title"]
     headers = csv.pop(0).split(',')
     chart.x_title = headers[0]
     chart.y_title = headers[1]
@@ -64,10 +63,10 @@ def gen_graph(filename):
     chart.x_labels = x_labels
     chart.x_labels_major = x_labels_major
     chart.add("", y_data)
-    chart.render_to_file('../' + filename.replace('.csv', '.svg'))
+    chart.render_to_file('../' + g_file["filename"].replace('.csv', '.svg'))
 
 
 
 # Call gen_graph() for each file in the graph_files array
-for filename in graph_files:
-    gen_graph(filename)
+for g_file in graph_files:
+    gen_graph(g_file)
